6afe7996c1d8e852eb72ac7cf3c9319c25f66383
steven <steven.loofat@yahoo.fr>
2020-04-06 23:43:50 +0200

    [+] update using interpolation for credential

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
+variable "ACCESS_KEY" {}
+variable "SECRET_KEY" {}
+
 provider "aws" {
   region     = "eu-west-3"
-  access_key = 
-  secret_key = 
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
 }
 
 resource "aws_ecs_cluster" "creating-basic-nodejs-app" {
   name = "basic-nodejs-app-cluster" #improve the name
 }
+
+# resource "aws_ecs_capacity_provider" "test-provider" {
+#   name = "basic-nodejs-app-capacity-provider"
+
+#   auto_scaling_group_provider {
+#     auto_scaling_group_arn = "aws_autoscaling_group.test-provider.arn"
+#   }
+# }
 
 resource "aws_ecr_repository" "creating-ecr-for-basic-nodejs-app" {
   name                 = "basic-nodejs-app-ecr"
@@ -19,16 +30,30 @@ resource "aws_ecr_repository" "creating-ecr-for-basic-nodejs-app" {
 
 resource "aws_ecs_task_definition" "creating-task-definition-for-basic-nodejs-app" {
   family                = "basic-nodejs-app-task-definition"
-  container_definitions = "${file("task-definitions/basic-nodejs-app-definition.json")}"
+  container_definitions = file("task-definitions/basic-nodejs-app-definition.json")
 
   volume {
     name = "basic-nodejs-app-storage"
   }
 }
-
 resource "aws_ecs_service" "basic-nodejs-app-service" {
   name            = "basic-nodejs-app-service"
   cluster         = "basic-nodejs-app-cluster"
-  task_definition = "${aws_ecs_task_definition.creating-task-definition-for-basic-nodejs-app.arn}"
+  task_definition = aws_ecs_task_definition.creating-task-definition-for-basic-nodejs-app.arn
+  # default_capacity_provider_strategy {
+  #   capacity_provider
+  # }
   # launch_type     = "FARGATE"
 }
+
+# resource "aws_placement_group" "basic-nodejs-app-service-group" {
+#   name = "test"
+#   strategy = "cluster"
+# }
+
+# resource "aws_autoscaling_group" "basic-nodejs-app-service-group" {
+#   name = "basic-nodejs-app-service-group"
+#     max_size                  = 5
+#   min_size                  = 2
+
+# }
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_access_key = "AKIAWQK75DOWHB4ACGV5"
-aws_secret_key = "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"
